c40eb33390e46866fa9c46c8de8cb26c0a575338
Incognito <lada.kashubo@yandex.ru>
2026-04-15 22:26:02 +0300

    start project. connection to backend

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [message, setMessage] = useState('');
+
+  const callBackend = async () => {
+    const response = await fetch('https://localhost:7102/api/values');
+    const data = await response.json();
+    setMessage(data.message);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Typing Trainer</h1>
+      <button onClick={callBackend}>Get Hello from Backend</button>
+      {message && <p>Ответ от бэкенда: {message}</p>}
     </div>
   );
 }
